187736547259adfe297d6e75b76d9f50868c6f49
JackDGriff <jack.griffiths@idwalmarine.com>
2025-08-11 21:38:47 +0100

    Clean up linter problems

--- travel-app/src/components/RoutePlanner/RoutePlannerSearch.jsx
+++ travel-app/src/components/RoutePlanner/RoutePlannerSearch.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import "./RoutePlannerSearch.css";
 import { LocationInput } from "../LocationInput/LocationInput";
